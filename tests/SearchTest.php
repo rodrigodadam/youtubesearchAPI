3d8852ee5e56a34d0d7dc73aedf9aa12e8397a6a
rodrigodadam <rodrigodadam@gmail.com>
2021-10-07 14:15:19 +0000

    Add SearchTest

---
+++ tests/SearchTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class SearchTest extends KernelTestCase
+{
+
+    public function testItWorks()
+    {
+        $this->assertTrue(true);
+    }
+
+}
